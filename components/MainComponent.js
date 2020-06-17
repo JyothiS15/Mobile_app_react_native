f5df2d721c32ab0ecccdbacfbf92c6e2e9828d2d
JyothiS15 <jyothi.s1798@gmail.com>
2020-06-17 12:13:37 +0530

    Picking an Image

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -30,21 +30,22 @@ const mapDispatchToProps = dispatch => ({
 });
 
 const LoginNavigator = createStackNavigator({
-      Login: { screen: Login }
-    }, {
-    navigationOptions: ({ navigation }) => ({
-      headerStyle: {
-          backgroundColor: "#512DA8"
-      },
-      headerTitleStyle: {
-          color: "#fff"
-      },
-      headerTintColor: "#fff",
-      headerLeft: <Icon name="menu" size={24}
-        iconStyle={{ color: 'white' }}
-        onPress={ () => navigation.toggleDrawer() } />
-    })
-  });
+    Login: Login
+  }, {
+  navigationOptions: ({ navigation }) => ({
+    headerStyle: {
+        backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+        color: "#fff"
+    },
+    title: 'Login',
+    headerTintColor: "#fff",
+    headerLeft: <Icon name="menu" size={24}
+      iconStyle={{ color: 'white' }}
+      onPress={ () => navigation.toggleDrawer() } />
+  })
+});
 
 const ReservationNavigator = createStackNavigator({
   Reservation: { screen: Reservation }
